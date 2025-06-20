3838e773d338cda8c05f19bbc1ce6abe6f880566
Vlad Mikryukov <mikryukov811@mail.ru>
2025-06-20 23:16:01 +0400

    added swiper bundle css

--- js/swiper.js
+++ js/swiper.js
@@ -1,13 +1,13 @@
 export function initSwiper() {
-  const swiper = new Swiper(".mySwiper", {
+  var swiper = new Swiper(".mySwiper", {
     spaceBetween: 10,
     slidesPerView: 3,
     freeMode: true,
     watchSlidesProgress: true,
   });
-  const swiper2 = new Swiper(".mySwiper2", {
+  var swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
-    direction: "vertical",
+    direction: "horizontal",
     slidesPerView: 1,
     thumbs: {
       swiper: swiper,
