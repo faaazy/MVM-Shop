0dc5fde4099357832edfb897acc3d8d8f669f763
Vlad Mikryukov <mikryukov811@mail.ru>
2025-06-21 21:57:43 +0400

    fixed swiper and product page is halfway done

--- js/swiper.js
+++ js/swiper.js
@@ -2,13 +2,15 @@ export function initSwiper() {
   var swiper = new Swiper(".mySwiper", {
     spaceBetween: 10,
     slidesPerView: 3,
+    direction: "horizontal",
     freeMode: true,
     watchSlidesProgress: true,
   });
-  var swiper2 = new Swiper(".mySwiper2", {
+  var swiper2 = new Swiper(".mySwiperControls", {
     spaceBetween: 10,
-    direction: "horizontal",
+    direction: "vertical",
     slidesPerView: 1,
+    allowTouchMove: false,
     thumbs: {
       swiper: swiper,
     },
