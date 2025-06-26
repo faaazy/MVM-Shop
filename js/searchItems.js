e3c3c442c378479549b1cc62cb84d73d7dc2bbf0
Vlad Mikryukov <mikryukov811@mail.ru>
2025-06-26 23:56:26 +0400

    search

--- js/searchItems.js
+++ js/searchItems.js
@@ -2,13 +2,13 @@ import { debounce } from "./debounce.js";
 
 export function initSearchItems(productsData, showClickedPage) {
   const search = document.querySelector(".search");
-  const searchContainer = document.querySelector(".search__grid");
 
   const debouncedSearch = debounce((value) => {
     const searchValue = value.trim().toLowerCase();
 
     if (searchValue == "") {
       showClickedPage(document.querySelector(".home"));
+      return;
     }
 
     const filteredProducts = productsData.filter((product) => {
@@ -24,14 +24,60 @@ export function initSearchItems(productsData, showClickedPage) {
       return false;
     });
 
-    console.log(filteredProducts);
+    renderSearchItems(filteredProducts);
 
-    showClickedPage(search);
+    showClickedPage(document.querySelector(".search"));
   }, 500);
 
   const searchInput = document.querySelector(".search__input");
 
   searchInput.addEventListener("input", () => {
     debouncedSearch(searchInput.value);
+  });
+}
+
+function renderSearchItems(products) {
+  const searchContainer = document.querySelector(".search__grid");
+
+  products.forEach((item) => {
+    const searchItemHTML = `
+        <div class="catalog__items-product">
+          <div class="placeholder"></div>
+          <div class="recent__item-img">
+            <img src="${item.images[0]}" alt="" class="recent__item-img__image" />
+          </div>
+          <div class="catalog__items-product__content">
+            <div class="recent__item-title">${item.title}</div>
+            <div class="recent__item-bottom">
+              <div class="recent__item-rating">
+                <div class="recent__item-rating__img">
+                  <img src="./img/star.svg" alt="" />
+                </div>
+                <div class="recent__item-rating__num">${item.rating}</div>
+                <div class="recent__item-rating__reviews">${item.reviews.length} reviews</div>
+              </div>
+              <div class="recent__item-cta">
+                <div class="recent__item-cta__price">$ ${item.price}</div>
+                <div class="recent__item-cta__favorite recent__item-cta__img">
+                  <img src="./img/heart.svg" alt="" />
+                </div>
+                <div class="recent__item-cta__cart recent__item-cta__img">
+                  <img src="./img/cart.svg" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    `;
+
+    searchContainer.insertAdjacentHTML("beforeend", searchItemHTML);
+
+    const img = document.querySelector(".catalog__items-product img");
+    img.style.display = "none";
+
+    img.onload = () => {
+      document.querySelector(".placeholder").remove();
+      img.style.display = "inline";
+    };
   });
 }
